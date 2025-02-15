e3baffb9359a7f007841fc34f91a895a9d58f96e
kurobane <darkflamemaster663@gmail.com>
2025-02-15 15:53:48 -0300

    pipe custom errors with tags

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,31 @@
-import { Effect } from "effect";
+import { Effect, pipe } from "effect";
 
-const fetchRequest = Effect.tryPromise(() =>
-  fetch("https://pokeapi.co/api/v2/pokemon/garchomp/"),
-);
+interface FetchError {
+  readonly _tag: "FetchError";
+}
+interface JsonError {
+  readonly _tag: "JsonError";
+}
+
+const fetchRequest = Effect.tryPromise({
+  try: () => fetch("https://pokeapi.co/api/v2/psadokemon/garchomp/"),
+  catch: (): FetchError => ({ _tag: "FetchError" }),
+});
 const jsonResponse = (response: Response) =>
-  Effect.tryPromise(() => response.json());
+  Effect.tryPromise({
+    try: () => response.json(),
+    catch: (): JsonError => ({ _tag: "JsonError" }),
+  });
 
-const main = Effect.flatMap(fetchRequest, jsonResponse);
+const savePokemon = (pokemon: unknown) =>
+  Effect.tryPromise({
+    try: () => fetch("/api/pokemon", { body: JSON.stringify(pokemon) }),
+    catch: (): FetchError => ({ _tag: "FetchError" }),
+  });
 
-Effect.runPromise(main);
+const main = fetchRequest.pipe(
+  Effect.flatMap(jsonResponse),
+  Effect.flatMap(savePokemon),
+);
+
+Effect.runPromise(main).then(console.log);
