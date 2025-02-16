d641bbc5e43c9d5948e58c298aed2969819758d1
Icaro <darkflamemaster663@gmail.com>
2025-02-16 17:17:40 -0300

    separete program definition from error handling

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,29 @@
 import { Data, Effect, pipe } from "effect";
 
 class FetchError extends Data.TaggedError("FetchError")<{}> {}
-
 class JsonError extends Data.TaggedError("JsonError")<{}> {}
 
 const fetchRequest = Effect.tryPromise({
   try: () => fetch("https://pokeapi.co/api/v2/psadokemon/garchomp/"),
   catch: () => new FetchError(),
 });
+
 const jsonResponse = (response: Response) =>
   Effect.tryPromise({
     try: () => response.json(),
     catch: () => new JsonError(),
   });
 
-const main = fetchRequest.pipe(
-  Effect.filterOrFail(
-    (response) => response.ok,
-    () => new FetchError(),
-  ),
-  Effect.flatMap(jsonResponse),
+const program = Effect.gen(function* () {
+  const response = yield* fetchRequest;
+  if (!response.ok) {
+    return yield* new FetchError();
+  }
+
+  return yield* jsonResponse(response);
+});
+
+const main = program.pipe(
   Effect.catchTags({
     FetchError: () => Effect.succeed("Fetch error"),
     JsonError: () => Effect.succeed("Json error"),
